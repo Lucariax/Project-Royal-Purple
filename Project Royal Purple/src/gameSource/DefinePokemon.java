9c28b1862131d625374fe94f04a2ca8e91d3edc1
Lucariax <lucariax@hotmail.com>
2017-08-29 09:39:49 -0400

    added more Pokemon stuff
    stuffy stuff stuff

---
+++ Project Royal Purple/src/gameSource/DefinePokemon.java
@@ -0,0 +1,7 @@
+package gameSource;
+
+public class DefinePokemon {
+	public DefinePokemon() {
+		Pokemon Lucario = new Pokemon("Lucario", 70, 110, 70, 115, 70, 90, 3, 4, 3, 4, 3, 3, 20);
+	}
+}
--- Project Royal Purple/src/gameSource/Pokemon.java
+++ Project Royal Purple/src/gameSource/Pokemon.java
@@ -1,6 +1,69 @@
 package gameSource;
 
 public class Pokemon {
-	private static int hitPoints, experience, attack, defense, specialAttack, specialDefense, speed;
-	private static int evHitPoints, evAttack, evDefense, evSpecialAttack, evSpecialDefense, evSpeeed, experienceModifier;
+	private static int hitPoints, experience, attack, defense, specialAttack, specialDefense, speed, level;
+	private static int evHitPoints, evAttack, evDefense, evSpecialAttack, evSpecialDefense, evSpeed, totalExperience, experienceCap;
+	private static String name, nickname, displayName;
+	private static double experienceModifier;
+	public Pokemon(String n, int h, int a, int d, int sA, int sD, int s, int evH, int evA, int evD, int evSA, int evSD, int evS, int l) {
+		name = n;
+		nickname = n;
+		displayName = n;
+		level = l;
+		
+		hitPoints = h;
+		attack = a;
+		specialAttack = sA;
+		defense = d;
+		specialDefense = sD;
+		speed = s;
+		
+		totalExperience = 0;
+		experienceModifier = 1.6;
+		
+		evHitPoints = evH;
+		evAttack = evA;
+		evSpecialAttack = evSA;
+		evDefense = evD;
+		evSpecialDefense = evSD;
+		evSpeed = evS;
+	}
+	public void addExperience(int exp) {
+		totalExperience += exp;
+		if(totalExperience >= experienceCap && level == 100) {
+			totalExperience = experienceCap;
+		}
+		else if(totalExperience >= experienceCap) {
+			totalExperience -= experienceCap;
+			levelUp();
+		}
+	}
+	public void levelUp() {
+		hitPoints += evHitPoints;
+		attack += evAttack;
+		specialAttack += evSpecialAttack;
+		defense += evDefense;
+		specialDefense += evSpecialDefense;
+		speed += evSpeed;
+		level++;
+	}
+	public void setNickname(String n) {
+		nickname = n;
+		displayName = n;
+	}
+	public static String getName() {
+		return name;
+	}
+	public static String getNickName() {
+		return nickname;
+	}
+	public static int getLevel() {
+		return level;
+	}
+	public static int getTotalExperience() {
+		return totalExperience;
+	}
+	public static int getExperienceCap() {
+		return experienceCap;
+	}
 }
